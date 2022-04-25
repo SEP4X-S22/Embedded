79bf5af1ddf671945b94cb68d4a857becb908399
SkyKalazar <74299324+SkyKalazar@users.noreply.github.com>
2022-04-25 21:40:55 +0100

    InitialProjectCreation
    FIles that are generated after creating basic C executable project in Microchip studio

---
+++ SEP4/main.c
@@ -0,0 +1,18 @@
+/*
+ * SEP4.c
+ *
+ * Created: 25/04/2022 21:40:03
+ * Author : Lukas
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
