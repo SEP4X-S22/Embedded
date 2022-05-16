223ba80ec929be437117f47a4110e56c7b601197
NoHop3 <u_sux@abv.bg>
2022-05-16 09:58:44 +0200

    Initializing the semaphore (sharedMutex)

--- SEP4/main.c
+++ SEP4/main.c
@@ -29,7 +29,7 @@ void task1( void *pvParameters );
 void task2( void *pvParameters );
 
 // define semaphore handle
-SemaphoreHandle_t xTestSemaphore;
+SemaphoreHandle_t sharedMutex;
 QueueHandle_t xQueue;
 
 // Initialize WakeTime and Frequency variables
@@ -45,14 +45,11 @@ void create_tasks_and_semaphores(void)
 	// Semaphores are useful to stop a Task proceeding, where it should be paused to wait,
 	// because it is sharing a resource, such as the Serial port.
 	// Semaphores should only be used whilst the scheduler is running, but we can set it up here.
-	/*if ( xTestSemaphore == NULL )  // Check to confirm that the Semaphore has not already been created.
+	if ( sharedMutex == NULL )  // Check to confirm that the Semaphore has not already been created.
 	{
-		xTestSemaphore = xSemaphoreCreateMutex();  // Create a mutex semaphore.
-		if ( ( xTestSemaphore ) != NULL )
-		{
-			xSemaphoreGive( ( xTestSemaphore ) );  // Make the mutex available for use, by initially "Giving" the Semaphore.
-		}
-	}*/
+		sharedMutex = xSemaphoreCreateMutex();  // Create a mutex semaphore.
+		// xSemaphoreGive(sharedMutex); - redundant because a mutex is given when it is created
+	}
 
 	xTaskCreate(
 	task1
